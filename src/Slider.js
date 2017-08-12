4c8a62379b0c606777318edafd6c2497c9cf8340
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-12 13:32:06 -0500

    Created slider component.

---
+++ src/Slider.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Slider.css';
+
+class Slider extends Component {
+    render() {
+        return (
+            <div className='Board'>
+                <div className='green'></div>
+                <div className='red'></div>
+                <div className='yellow'></div>
+                <div className='blue'></div>
+            </div>
+        );
+    }
+}
+
