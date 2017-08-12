984b9a17c348f02f695afb1c4a35c72c54aa201e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-12 13:55:02 -0500

    Added constructor and state to app.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import Board from './Board';
 import ControlPanel from './ControlPanel';
 import './App.css';
+import simonGame from 'simon-game';
 
-const props = {
-    count: 4
-};
+const simon = simonGame();
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Board />
-        <ControlPanel count={props.count} />
-      </div>
-    );
-  }
+    constructor (props) {
+        super(props);
+        this.state = {
+            isOn: false,
+            isStrict: false,
+            count: '--',
+            guesses: simon.getGuesses(),
+            currentSeries: simon.getCurrent()
+        };
+    } 
+
+    render () {
+        return (
+            <div className="App">
+                <Board />
+                <ControlPanel count={this.state.count} />
+            </div>
+        );
+    }
 }
 
 export default App;
