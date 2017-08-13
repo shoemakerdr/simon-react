9fc9d6f4669eec4e3788afd23d5a183cbd269db3
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-13 17:06:21 -0500

    Added styles for each space on board when active and inactive.

---
+++ src/BoardStyles.js
@@ -0,0 +1,26 @@
+
+export const green = {
+    backgroundColor: 'green'
+}
+
+export const activeGreen = {
+    backgroundColor: 'lightgreen'
+}
+export const red = {
+    backgroundColor: 'red'
+}
+export const activeRed = {
+    backgroundColor: 'pink'
+}
+export const yellow = {
+    backgroundColor: 'yellow'
+}
+export const activeYellow = {
+    backgroundColor: 'lightyellow'
+}
+export const blue = {
+    backgroundColor: 'blue'
+}
+export const activeBlue = {
+    backgroundColor: 'lightblue'
+}
