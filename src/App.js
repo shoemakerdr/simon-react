f78494dc5aa4ede24b1175bbbffbd9cae83f5fd9
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-13 18:07:36 -0500

    Added playSeries method that activates buttons in current series.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         this.setStrict = this.setStrict.bind(this)
         this.start = this.start.bind(this)
         this.playSeries = this.playSeries.bind(this)
+        this.activateSpace = this.activateSpace.bind(this)
+        this.iterateSeries = this.iterateSeries.bind(this)
+        this.playSeries = this.playSeries.bind(this)
         this.state = {
             isOn: false,
             isStrict: false,
@@ -25,6 +28,10 @@ class App extends Component {
                 red: false,
                 yellow: false,
                 blue: false
+            },
+            timerIDs: {
+                activate: null,
+                interval: null
             }
         }
     }
@@ -51,6 +58,7 @@ class App extends Component {
                 guesses: simon.getGuesses(),
                 currentSeries: simon.getCurrent()
             })
+            this.playSeries(this.state.currentSeries)
         }
         if (this.state.isOn && !this.state.hasStarted) {
             this.setState({
@@ -59,15 +67,49 @@ class App extends Component {
                 guesses: simon.getGuesses(),
                 currentSeries: simon.getCurrent()
             })
+            this.playSeries(this.state.currentSeries)
         }
     }
 
-    playSeries () {
-        this.state.currentSeries.forEach(move => console.log(move))
-        simon.next()
+    activateSpace(space) {
+        const color = [null, 'green', 'red', 'yellow', 'blue']
+        const activateID = setTimeout(() => {
+            this.setState({
+                colors: {[color[space]]: true}
+            })
+            setTimeout(() => {
+                this.setState({
+                    colors: {
+                        green: false,
+                        red: false,
+                        yellow: false,
+                        blue: false
+                    }
+                })
+            },1000)
+        },1000)
         this.setState({
-            count: simon.getCount(),
-            currentSeries: simon.getCurrent()
+            timerIDs: {
+                activate: activateID
+            }
+        })
+    }
+
+    iterateSeries (iterator) {
+        const next = iterator.next().value
+        if (next !== undefined) {
+            this.activateSpace(next[1])
+        }
+        else clearInterval(this.timerIDs.interval)
+    }
+
+    playSeries (series) {
+        const seriesIterator = series.entries()
+        const intervalID = setInterval(this.iterateSeries(seriesIterator),1000)
+        this.setState({
+            timerIDs: {
+                interval: intervalID
+            }
         })
     }
 
