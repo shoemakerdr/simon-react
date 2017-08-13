21a19a6aac67853b0a98dda65ebffe7f4ab9860a
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-13 17:08:02 -0500

    Changed to stateless component. Imported board space active/inactive styles.

--- src/Board.js
+++ src/Board.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
-import './Board.css';
+import React from 'react'
+import './Board.css'
+import {green, activeGreen, red, activeRed, yellow, activeYellow, blue, activeBlue} from './BoardStyles'
 
-class Board extends Component {
-    render() {
-        return (
-            <div className='Board'>
-                <div className='green'></div>
-                <div className='red'></div>
-                <div className='yellow'></div>
-                <div className='blue'></div>
-            </div>
-        );
-    }
+const Board = props => {
+    return (
+        <div className='Board'>
+            <div
+                style={props.colors.green ? activeGreen : green}
+                className='green'></div>
+            <div
+                style={props.colors.red ? activeRed : red}
+                className='red'></div>
+            <div
+                style={props.colors.yellow ? activeYellow : yellow}
+                className='yellow'></div>
+            <div
+                style={props.colors.blue ? activeBlue : blue}
+                className='blue'></div>
+        </div>
+    )
 }
 
-export default Board;
+export default Board
