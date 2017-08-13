afe90d72791896e7642ba677a1fbce69b00cb949
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-12 23:06:28 -0500

    Added method to change slider style.

--- src/Slider.js
+++ src/Slider.js
@@ -2,12 +2,47 @@ import React, { Component } from 'react';
 import './Slider.css';
 
 class Slider extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            isOn: props.isOn,
+        }
+        this.setSlider = this.setSlider.bind(this)
+    }
+
+    setSlider() {
+        this.setState({
+            isOn: !this.state.isOn
+        })
+        console.log(this.state.isOn)
+    }
+
+    onOrOff (isOn) {
+        const onStyle = {
+            width: '40%',
+            height: '100%',
+            backgroundColor: 'lightblue',
+            position: 'absolute',
+            right:'null'
+        }
+        const offStyle = {
+            width: '40%',
+            height: '100%',
+            backgroundColor: 'lightblue',
+            position: 'absolute',
+            right: '0'
+        }
+        return isOn ? onStyle : offStyle
+    }
+
     render() {
+        const isRight= (!this.state.isOn) ? '0' : 'null'
+        console.log(isRight)
         return (
-            <div className='Slider'>
+            <div className='Slider' onClick={this.setSlider}>
                 <p>{' ON'}</p>
                 <div className='switch-path'>
-                    <div className='switch'></div>
+                    <div className='switch' style={{right: isRight}}></div>
                 </div>
                 <p>{'OFF'}</p>
             </div>
