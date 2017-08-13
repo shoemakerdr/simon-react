46ebd875a9e98610d6cd13557536843c8e84f779
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-12 23:29:35 -0500

    Changed to functional stateless component. Added slider changed onClick.

--- src/Slider.js
+++ src/Slider.js
@@ -1,53 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Slider.css';
 
-class Slider extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            isOn: props.isOn,
-        }
-        this.setSlider = this.setSlider.bind(this)
-    }
+const onStyle = {
+    right:'60%',
+    left: '0'
+}
+const offStyle = {
+    right:'0',
+    left: '60%'
+}
 
-    setSlider() {
-        this.setState({
-            isOn: !this.state.isOn
-        })
-        console.log(this.state.isOn)
-    }
-
-    onOrOff (isOn) {
-        const onStyle = {
-            width: '40%',
-            height: '100%',
-            backgroundColor: 'lightblue',
-            position: 'absolute',
-            right:'null'
-        }
-        const offStyle = {
-            width: '40%',
-            height: '100%',
-            backgroundColor: 'lightblue',
-            position: 'absolute',
-            right: '0'
-        }
-        return isOn ? onStyle : offStyle
-    }
-
-    render() {
-        const isRight= (!this.state.isOn) ? '0' : 'null'
-        console.log(isRight)
-        return (
-            <div className='Slider' onClick={this.setSlider}>
-                <p>{' ON'}</p>
-                <div className='switch-path'>
-                    <div className='switch' style={{right: isRight}}></div>
-                </div>
-                <p>{'OFF'}</p>
+const Slider = props => {
+    return (
+        <div className='Slider' onClick={props.setOnOff}>
+            <p>{'ON'}</p>
+            <div className='switch-path'>
+                <div className='switch' style={props.isOn ? onStyle : offStyle}></div>
             </div>
-        );
-    }
+            <p>{'OFF'}</p>
+        </div>
+    )
 }
 
 export default Slider;
