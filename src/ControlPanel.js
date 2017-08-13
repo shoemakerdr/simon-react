e30408a1d1d8eed93fc13c06da316c2e423fb34d
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-12 23:05:35 -0500

    Added conditional rendering based on props. Changed to stateless component.

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ControlPanel.css';
 import Slider from './Slider';
 
-class ControlPanel extends Component {
-    render() {
-        return (
-            <div className='ControlPanel'>
-                <div className='wrapper'>
-                    <div className='count'>{this.props.count}</div>
+
+const strictStyle = {
+    color: 'pink',
+    backgroundColor: 'red',
+    border: '.1em solid pink'
+}
+
+const notStrictStyle = {}
+
+const isStrictStyle = isStrict => isStrict ? strictStyle : notStrictStyle
+
+const padCount = count => (count === '--' || count > 9) ? count : `0${count}`
+
+const ControlPanel = props => {
+    return (
+        <div className='ControlPanel'>
+            <div className='wrapper'>
+                <div className='count'>{padCount(props.count)}</div>
+                <div className='buttons'>
                     <button className='start'>Start</button>
-                    <button className='strict'>Strict</button>
-                    <Slider />
+                    <button className='strict' style={isStrictStyle(props.isStrict)}>Strict</button>
                 </div>
+                <Slider
+                    isOn={props.isOn}
+                    setOnOff={props.setOnOff}
+                />
             </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default ControlPanel;
