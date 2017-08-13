c962af8725db0ff5e7cbc54c03c869d4775494f3
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-13 17:07:21 -0500

    Added playSeries method and colors object to state.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,20 @@ class App extends Component {
         this.setOnOff = this.setOnOff.bind(this)
         this.setStrict = this.setStrict.bind(this)
         this.start = this.start.bind(this)
+        this.playSeries = this.playSeries.bind(this)
         this.state = {
             isOn: false,
             isStrict: false,
             hasStarted: false,
             count: '--',
             guesses: simon.getGuesses(),
-            currentSeries: simon.getCurrent()
+            currentSeries: simon.getCurrent(),
+            colors: {
+                green: false,
+                red: false,
+                yellow: false,
+                blue: false
+            }
         }
     }
 
@@ -31,20 +38,45 @@ class App extends Component {
     setOnOff () {
         this.setState({
             isOn: !this.state.isOn,
-            hasStarted: !this.state.isOn ? false : this.state.hasStarted
+            hasStarted: !this.state.isOn ? false : this.state.hasStarted,
+            count: '--'
         })
     }
 
     start () {
+        if (this.state.isOn && this.state.hasStarted) {
+            simon.newSeries()
+            this.setState({
+                count: simon.getCount(),
+                guesses: simon.getGuesses(),
+                currentSeries: simon.getCurrent()
+            })
+        }
+        if (this.state.isOn && !this.state.hasStarted) {
+            this.setState({
+                hasStarted: true,
+                count: simon.getCount(),
+                guesses: simon.getGuesses(),
+                currentSeries: simon.getCurrent()
+            })
+        }
+    }
+
+    playSeries () {
+        this.state.currentSeries.forEach(move => console.log(move))
+        simon.next()
         this.setState({
-            hasStarted: (this.state.isOn && !this.state.hasStarted) ? true : false
+            count: simon.getCount(),
+            currentSeries: simon.getCurrent()
         })
     }
 
     render () {
         return (
             <div className="App">
-                <Board />
+                <Board
+                    colors={this.state.colors}
+                    playSeries={this.playSeries}/>
                 <ControlPanel
                     count={this.state.count}
                     isOn={this.state.isOn}
