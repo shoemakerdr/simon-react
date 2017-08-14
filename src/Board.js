b0a7e57718e20e0ed55053030a15c3cda2b144ef
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-13 22:24:56 -0500

    Added onClick handlers for each space.

--- src/Board.js
+++ src/Board.js
@@ -6,15 +6,19 @@ const Board = props => {
     return (
         <div className='Board'>
             <div
+                onClick={() => props.checkUserInput(1)}
                 style={props.colors.green ? activeGreen : green}
                 className='green'></div>
             <div
+                onClick={() => props.checkUserInput(2)}
                 style={props.colors.red ? activeRed : red}
                 className='red'></div>
             <div
+                onClick={() => props.checkUserInput(3)}
                 style={props.colors.yellow ? activeYellow : yellow}
                 className='yellow'></div>
             <div
+                onClick={() => props.checkUserInput(4)}
                 style={props.colors.blue ? activeBlue : blue}
                 className='blue'></div>
         </div>
